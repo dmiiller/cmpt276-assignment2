463f3756e37d54d7c435a9b13f84677d4a1acb0a
David <dpmii@hotmail.ca>
2016-06-07 16:10:11 -0700

    Push check

---
+++ public/student.js
@@ -0,0 +1,31 @@
+function filt(){
+	
+}
+
+//*MAIN*//
+//console.log("Test"); //Log Test
+
+//PercentageLoop
+
+	//Declarations
+	
+   
+//onClicks
+document.getElementById('avg').onclick=average;
+document.getElementById('mean').onclick=mean;
+   	
+//blurs 
+document.getElementById("a1").addEventListener("blur", update);
+document.getElementById("a2").addEventListener("blur", update);
+document.getElementById("b1").addEventListener("blur", update);
+document.getElementById("b2").addEventListener("blur", update);
+document.getElementById("c1").addEventListener("blur", update);
+document.getElementById("c2").addEventListener("blur", update);
+document.getElementById("d1").addEventListener("blur", update);
+document.getElementById("d2").addEventListener("blur", update);
+   	
+  
+
+
+
+
